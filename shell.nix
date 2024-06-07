df10d64facba6c25908b7d3a29b393c66feedead
Larken Barnes <larken@barnes.pro>
2024-06-06 22:45:26 -0400

    Add: shell.nix

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+let
+  vimrcConfig = {
+    customRC = ''
+      "Start terminal if not open in file
+      autocmd VimEnter * if empty(bufname(''')) | exe "terminal" | endif
+      autocmd BufEnter * let buf=bufname() | if isdirectory(buf) | exec 'terminal' | call feedkeys('icd' ".buf."\<CR> | endif
+
+      set nu
+      colorscheme molokai
+      set tabstop=2
+      set shiftwidth=2
+    '';
+    
+    packages.myPackages = with pkgs.vimPlugins; {
+      start = [
+        # Style
+        vim-colorschemes
+        vim-airline 
+        vim-airline-themes
+
+        # Errors
+        ale 
+      ];
+    };
+  };
+in
+pkgs.mkShell {
+  name = "devkit";
+  buildInputs = [
+    (pkgs.neovim.override {
+      configure = vimrcConfig; 
+    })
+  ];
+  shellHook = ''
+    echo "Entered Dev Kit"
+  '';
+}
